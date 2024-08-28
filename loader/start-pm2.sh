07fa6b97eda7532004392dfa305d9268e4d86d71
Maxim-Lanskoy <lanskoyma@gmail.com>
2024-08-28 03:41:39 +0300

    Minor improvement.

--- loader/start-pm2.sh
+++ loader/start-pm2.sh
@@ -3,7 +3,11 @@
 # Load the main environment variables from the .env file located one level up
 set -o allexport
 while IFS='=' read -r key value; do
-    if [[ $key != '#'* ]]; then
+    # Only export if key is not empty and doesn't start with a #
+    if [[ ! -z "$key" && "$key" != \#* ]]; then
+        # Remove leading and trailing spaces from key and value
+        key=$(echo $key | xargs)
+        value=$(echo $value | xargs)
         export "$key=$value"
     fi
 done < ../.env
@@ -25,7 +29,9 @@ do
     # Load the environment variables for the specific exchange (now using the lowercase filename)
     set -o allexport
     while IFS='=' read -r key value; do
-        if [[ $key != '#'* ]]; then
+        if [[ ! -z "$key" && "$key" != \#* ]]; then
+            key=$(echo $key | xargs)
+            value=$(echo $value | xargs)
             export "$key=$value"
         fi
     done < ../.env.$EXCHANGE_LOWER
@@ -36,6 +42,9 @@ do
 
     # Generate the nostalgia-general-$EXCHANGE_LOWER.json by replacing placeholders in the template
     sed "s/secrets-config.json/secrets-config-$EXCHANGE_LOWER.json/g" user_data/nostalgia-general.json > user_data/nostalgia-general-$EXCHANGE_LOWER.json
+
+    # Create the trading mode config file
+    envsubst < user_data/trading_mode-template.json > user_data/trading_mode-${FREQTRADE__TRADING_MODE_TYPE}.json
 
     # Start Freqtrade with environment variables and using the exchange-specific config files
     pm2 start freqtrade --name "Freqtrade-$EXCHANGE" --interpreter python3 -- trade --config user_data/nostalgia-general-$EXCHANGE_LOWER.json --strategy "${FREQTRADE__STRATEGY_FILE_NAME}" --db-url "sqlite:///user_data/Nostalgy-${FREQTRADE__EXCHANGE__NAME}-${FREQTRADE__TRADING_MODE_TYPE}-DB.sqlite"
